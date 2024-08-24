6415f60e6b76523b22f3a76343d84a0dbb39c8ad
Millah <bsmsultani@gmail.com>
2024-08-24 10:29:27 +0000

    starting new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 // App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Sidebar from './Sidebar'; // Import the Sidebar component
-import Content from './Content'; // Import the Content component
-import Rightsidebar from './Righsidebar';
-import Viewer from './Viewer';
 import './App.css';
 
 // Main App component with routing
-const App = () => (
+
+const url = "http://127.0.0.1:8080"
+
+
+const App = () => {
+
+  useEffect(() => {
+    localStorage.setItem('url', url)
+  }, [])
+
+  
+  return (
   <Router>
     <div className="app">
-      <Sidebar />
-        <Routes>
-          <Route path="/" element={<p>Select a topic from the sidebar.</p>} />
-          <Route path="/topic/:topicId" element={<Content />} />
-          <Route path="/topic/:topicId/post/:postId" element={<Viewer />} />
-        </Routes>
-      <Rightsidebar />
+      <h1> Under development </h1>
     </div>
   </Router>
-);
+)};
 
 export default App;
