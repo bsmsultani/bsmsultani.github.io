73285860b4230254f440b52c1bdcecae921ff09e
bismillah <n11247282@qut.edu.au>
2025-02-26 18:29:51 +1000

    added a coursal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,17 @@ const FooterWrapper = styled.footer`
     width: 100%;
     min-height: 100px;
     background-color: white;
-    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
 `
 const Footer = () =>  {
     return (
         <FooterWrapper>
             <sub>Bismillah Sultani</sub>
+            <br></br>
             <sub>@2025</sub>
         </FooterWrapper>
     )
